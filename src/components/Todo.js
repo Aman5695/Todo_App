2635f6edb15a21eb845a887f8328f8a82480c77e
Aman5695 <amansharma9623139@gmail.com>
2022-01-05 18:54:32 +0530

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import logo from "../Images/todo.png";
 import {useEffect, useState} from 'react';
 
+
 const getLocalItems = () => {
     let items = localStorage.getItem('data');
     if(items) {
@@ -12,13 +13,31 @@ const getLocalItems = () => {
 const Todo = () => {
   const [inputData, setInputData] = useState('');
     const [item, setItem] = useState(getLocalItems());
-
+    const [toggleBtn, setToggleBtn] = useState(true)
+  const [isEditItem, setIsEditItem] = useState(null)
 
   const addItem = ()=> {
       if(!inputData) {
+        alert('Fill the data');
+      } else if(inputData && !toggleBtn){
+        setItem(
+          item.map((elem) => {
+            if (elem.id === isEditItem){
+                return{ ...elem, name:inputData}
+            }
+            return elem;
+          })
+        
+        )
+        setToggleBtn(true);
 
-      } else {
-        setItem([ ...item, inputData]);
+        setInputData('');
+    
+        setIsEditItem(null);
+      }
+       else {
+        const allInputData = { id: new Date().getTime().toString(), name:inputData}
+        setItem([ ...item,allInputData]);
         setInputData('');
       }
    
@@ -30,13 +49,25 @@ const submit = (e) => {
         addItem();    }
 }
 
-  const deleteItem = (id) => {
-    console.log(id);
-    const updateItem = item.filter((elem, ind) => {
-        return ind !== id;
+  const deleteItem = (index) => {
+    // console.log(id);
+    const updateItem = item.filter((elem) => {
+        return index !== elem.id;
 
     });
     setItem(updateItem);
+  }
+
+  const editItem= (id)=> {
+    let newEditItem = item.find((elem)=> {
+      return elem.id === id
+    });
+    console.log(newEditItem);
+    setToggleBtn(false);
+
+    setInputData(newEditItem.name);
+
+    setIsEditItem(id);
   }
 
   const removeAll = ()=> {
@@ -58,15 +89,24 @@ const submit = (e) => {
           </figure>
           <div className="addItems">
             <input type="text"  placeholder="Add items here... " value={inputData} onKeyDown={(e)=> submit(e)} onChange={(e) => setInputData(e.target.value)} />
-            <i className="fas fa-plus add-btn" title="Add item" onClick={addItem} type="submit"></i>
+            {
+              toggleBtn ?  <i className="fas fa-plus add-btn" title="Add item" onClick={addItem} type="submit"></i> :  
+                           <i className="far fa-edit add-btn" title="Update item" onClick={addItem}></i>
+
+            }
+           
           </div>
           <div className="showitems">
           {
-              item.map((elem, ind) => {
+              item.map((elem) => {
                   return(
-                    <div className="eachitem" key={ind}>
-              <h2>{elem}</h2>
-              <i className="far fa-trash-alt add-btn" title="Delete item" onClick={() => deleteItem(ind)}></i>
+                    <div className="eachitem" key={elem.id}>
+              <h2>{elem.name}</h2>
+              <div className="todo-btn">
+              <i className="far fa-edit add-btn" title="Edit item" onClick={() => editItem(elem.id)}></i>
+
+              <i className="far fa-trash-alt add-btn" title="Delete item" onClick={() => deleteItem(elem.id)}></i>
+              </div>
             </div>
                   )
               })
